b8357dc116b335f64f040c362d9095059384cae4
Sam Selikoff <sam.selikoff@gmail.com>
2020-05-29 09:38:44 -0400

    no need for css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./styles/index.css";
+import "tailwindcss/tailwind.css";
 import "./server";
 
 ReactDOM.render(<App />, document.getElementById("root"));
