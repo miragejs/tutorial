4ede71221cde676a768429413c7f51dc74142302
Sam Selikoff <sam.selikoff@gmail.com>
2020-06-26 10:03:51 -0400

    Fix default server

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { QueryParamProvider } from "use-query-params";
 import makeServer from "./server";
 import makeFinalServer from "./lib/server-final";
 
-makeFinalServer(); // For a live demo when deploying to Vercel
 if (
   process.env.NODE_ENV === "development" &&
   typeof makeServer === "function"
 ) {
-  // makeServer(); // For people following the tutorial
+  makeServer(); // For people following the tutorial
 } else if (process.env.NODE_ENV === "production") {
   makeFinalServer(); // For a live demo when deploying to Vercel
 }
